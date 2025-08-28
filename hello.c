1ab78f3be25e3e480090d5d6ee83b5ab0c17aea2
Orrin <owellman2@huskers.unl.edu>
2025-08-28 10:10:07 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author Orrin Wellman
+ *        owellman2@huskers.unl.edu
+ * Date: 8/28/25
+ *
+ * My Hack Hello.c file
+ *
+ */
+
+ #include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+   printf("Computer Engineering!\n");
+
+    return 0;
+}
